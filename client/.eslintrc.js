eb1f07e402ecb317af7e1a848edab0c84e10f0b5
jungwon <gardenlike.p@gmail.com>
2022-09-14 18:52:57 +0200

    add package

---
+++ client/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false, // <== ADD THIS
+  },
+};
