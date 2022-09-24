06cb10fe7424249e7912e7fc9b14b477743efeb0
jungwon <gardenlike.p@gmail.com>
2022-09-24 10:37:05 +0200

    progress ArtworkContext

--- client/App.js
+++ client/App.js
@@ -1,31 +1,25 @@
 import 'react-native-gesture-handler';
-import React, { useState } from 'react';
-// import { StyleSheet } from 'react-native';
+import React, { useState, useContext } from 'react';
+
+// SCREEN
 import Navigation from './src/components/Navigation';
 import LoginScreen from './src/screens/LoginScreen';
-// import { NavigationContainer } from '@react-navigation/native';
+
+// Context
 import { UserContext } from './src/UserContext';
+import { ArtworkContext } from './src/ArtworkContext';
 
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // TODO: ArtworkContext 구현하기!
+  // const { artworks, setArtworks } = useContext(ArtworkContext);
+  // setArtworks useState를 쓴 곳이 없다..? 나 바보??? db 서비스 부르는 곳에 추가해보자!! -> 찾음 ㅡㅡ.. 왜 안될까..
 
   return (
+    // <ArtworkContext.Provider value={{ artworks, setArtworks }}>
     <UserContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
-      {isLoggedIn ? (
-        <Navigation />
-      ) : (
-        <LoginScreen />
-        // <LoginScreen setIsLoggedIn={setIsLoggedIn} />
-      )}
+      {isLoggedIn ? <Navigation /> : <LoginScreen />}
     </UserContext.Provider>
+    // </ArtworkContext.Provider>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
